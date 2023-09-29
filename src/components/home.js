07a9d20b19ac98435ca29eb62420179f9b221185
hectordiazgomez <konlaptechs@gmail.com>
2023-09-29 17:34:48 -0500

    first

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div className="flex justify-center my-12">
+            <input className=" w-1/5 p-2 rounded outline-none border border-blue-600 text-gray-600 font-medium"/>
+        </div>
+    )
+}
+
+export default Home;
